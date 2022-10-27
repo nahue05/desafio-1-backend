1e649fcc5966a07f8f738387032813e3f008dbf0
nahuel <christiannahuelberrutti25@gmail.com>
2022-10-27 09:23:02 -0300

    correccion

--- index.js
+++ index.js
@@ -10,28 +10,29 @@ class Usuario {
         return (`Su nombre completo es: ${this.nombre} ${this.apellido}`);
     }
     addMascota(nombreMascota){
-        return(this.mascotas.push(nombreMascota))
+    (this.mascotas.push(nombreMascota))
     }
     countMascotas(){
         return(`las mascotas de ${this.nombre} son : ${this.mascotas.length}`);
     }
     addBook(nombreLibro, autorLibro){
-return(this.libros.push({nombreLibro, autorLibro}))   
+(this.libros.push({nombreLibro, autorLibro}))   
 }
     getBookNames(){
 const mapNombreLibros = this.libros.map(libro => libro.nombreLibro) 
-console.log(`${this.nombre} ${this.apellido} posee estos libros en su biblioteca ${mapNombreLibros}`);
+return console.log(mapNombreLibros);;
     }
 }
 
-const prueba = new Usuario("Nahuel", "Berrutti", [], [])
+const prueba = new Usuario("Nahuel", "Berrutti",[], [])
 console.log(prueba.getFullName());
 prueba.addMascota("lucy")
 prueba.addMascota("max")
 prueba.addMascota("morita")
 prueba.addMascota("duque")
 console.log(prueba.countMascotas());
-prueba.addBook("El sapo ruperto", "Roy berocay")
-prueba.addBook("Los cazaventura", "Susana Olaondo")
-prueba.getBookNames()
+prueba.addBook( "los Cazaventura", "Susana Olaondo")
+prueba.addBook( "El sapo Ruperto", "Roy Berocay")
+
+prueba.getBookNames();
 
